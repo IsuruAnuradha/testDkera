40d8dbf5c94d39127d6aa63f272b4ef788a27418
Isuru Anuradha <Isuruanuradha99@gmail.com>
2019-04-06 03:11:29 +0800

    code implementation for Question  three

---
+++ src/com/testDkera/question1/QuestionThree.java
@@ -0,0 +1,26 @@
+package com.testDkera.question1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QuestionThree {
+
+    public static void main(String[] args) {
+
+//        List<Integer> result = mergeTwoLists(  new ArrayList<>(Arrays.asList(1,2,6,7)) ,
+//                new ArrayList<>(Arrays.asList(4,5)));
+//        for(int i=0;i<result.size();i++){
+//            System.out.println(result.get(i));
+//        }
+    }
+
+    public static List<Integer> mergeTwoLists(List<Integer> A , List<Integer>B){
+
+        for (int index1 = 0 , index2=0 ; index2<B.size();index1++){
+            if(index1==A.size()||A.get(index1)>B.get(index2))
+                A.add(index1,B.get(index2++));
+        }
+        return A ;
+    }
+}
