24e1d1f353b1dca89443f6e5322449d0ece5b141
Isuru Anuradha <Isuruanuradha99@gmail.com>
2019-04-05 23:03:41 +0800

    code representation of Question one

---
+++ src/com/testDkera/question1/QuestionOne.java
@@ -0,0 +1,18 @@
+package com.testDkera.question1;
+
+public class QuestionOne {
+
+//    public static void main(String[] args) {
+//        System.out.println(findLargestElement(new int[]{23,100,45,67,6,78,55}));
+//    }
+
+    public static int findLargestElement(int[] arr){
+
+        int max= arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>max)
+                max=arr[i];
+        }
+        return max ;
+    }
+}
